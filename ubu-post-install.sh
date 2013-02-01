1ed14a8378aee3234fe32b96bc52381d4cadf8e2
Varun Kashyap <varunkashyapcse@gmail.com>
2013-02-01 22:07:07 +0530

    Added nodejs PPA and install. Removed unnecessary sudo for each command, run the script as sudo now

--- ubu-post-install.sh
+++ ubu-post-install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# PPA for latest node js
+add-apt-repository ppa:chris-lea/node.js  
+
+# update rep sources
+apt-get update
+
 ## Install useful packages:
 #	vim
 #	git
 #	vlc
-sudo apt-get install vim git vlc
+#	nodejs
+apt-get install vim git vlc nodejs
